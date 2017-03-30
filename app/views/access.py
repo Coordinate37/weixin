6c859d2fee3c65e97bb2be356073bb4fbfb5ea8c
whb <whb@macOS.localdomain>
2017-03-30 20:41:10 +0800

    抽离出check_signature函数

--- app/views/access.py
+++ app/views/access.py
@@ -6,38 +6,24 @@ from flask import request, make_response, Blueprint
 
 acc = Blueprint('acc', __name__)
 
-def log(data):
-    try:
-        fout = open('out.txt')
-        fout.write(data)
-        fout.close()
-    except IOError:
-        pass
-    except Exception:
-        pass
-
-@acc.route('/')
-def index():
-    return "Hello World!"
+def check_signature(data):
+    token = '1997117'
+    signature = data.get('signature', '')
+    timestamp = data.get('timestamp', '')
+    nonce = data.get('nonce', '')
+    echostr = data.get('echostr', '')
+    s = [timestamp, nonce, token]
+    s.sort()
+    s = ''.join(s)
+    if(hashlib.sha1(s).hexdigest() == signature):
+        return make_response(echostr)
 
 @acc.route('/weixin', methods=['GET','POST'])
 def wechat_auth():
     if request.method == 'GET':
-        print 'coming Get'
-        data = request.args
-        token = "1997117"
-        signature = data.get('signature','')
-        timestamp = data.get('timestamp','')
-        nonce = data.get('nonce','')
-        echostr = data.get('echostr','')
-        s = [timestamp, nonce, token]
-        s.sort()
-        s = ''.join(s)
-        if (hashlib.sha1(s).hexdigest() == signature):
-            return make_response(echostr)
-        return 'Token validate failed!'
-    log(request.data)
-    return request.data
+        return check_signature(request.args)
+    else:
+        return None
 
 if __name__ == '__main__':
     acc.run(host='0.0.0.0', debug=True)
