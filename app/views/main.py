de1a6de93f572528e53658e25f2169f8b3587b89
root <root@macOS.localdomain>
2017-04-03 18:34:35 +0800

    fix bugs

--- app/views/main.py
+++ app/views/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import request, make_response, Blueprint, jsonify
+from flask import request, make_response, Blueprint, jsonify, render_template
 from app.utils.utils import check_signature, create_menu, del_menu, get_signature
 from app.utils.response import wechat_response
 from app import app
@@ -27,3 +27,28 @@ def jsapi_auth():
     result = sign.sign()
     result['appId'] = app.config['APP_ID']
     return jsonify(result)
+
+@acc.route('/share.html', methods=['GET'])
+def get():
+    print request.url
+    signature = get_signature(request.url)
+    params = dict(title="微信分享尝试",
+                  link="http://newbbs.bingyan.net/topics",
+                  imgUrl="http://www.bingyan.net/static/img/team1_1.png",
+                  desc="微信自定义分享测试",
+                  appId=app.config['APP_ID']
+                  )
+    params.update(signature)
+    params['timestamp'] = str(params['timestamp'])
+    # transform params.values from utf-8 to unicode
+     
+    return render_template('share.html', 
+                            title=params['title'],
+                            link=params['link'],
+                            imgUrl=params['imgUrl'],
+                            desc=params['desc'],
+                            appId=params['appId'],
+                            signature=params['signature'],
+                            nonceStr=params['nonceStr'],
+                            timestamp=params['timestamp']
+                            )
